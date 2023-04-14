19920425d3e24ce5e175e0b22b95889f79a68a17
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-13 19:52:21 -0600

    Add game national drawing method and intialize with attributes

---
+++ spec/game_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+RSpec.describe Game do
+  before(:each) do 
+    @pick_4 = Game.new('Pick 4', 2)
+    @mega_millions = Game.new('Mega Millions', 5, true)
+  end
+
+  describe "initialize" do
+    it "exists and has attributes" do
+      expect(@pick_4).to be_a(Game)
+      expect(@pick_4.name).to eq("Pick 4")
+      expect(@pick_4.cost).to eq(2)
+      expect(@pick_4.national_drawing?).to be false
+      
+      expect(@mega_millions).to be_a(Game)
+      expect(@mega_millions.name).to eq("Mega Millions")
+      expect(@mega_millions.cost).to eq(5)
+      expect(@mega_millions.national_drawing?).to be true
+    end
+  end
+end
